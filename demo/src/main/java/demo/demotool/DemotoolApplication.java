71988fd140117ce47e0b39352f02da8c302e846b
Yassinesbr <essebbar.yassine@gmail.com>
2019-12-12 22:51:43 +0100

    first commit

---
+++ demo/src/main/java/demo/demotool/DemotoolApplication.java
@@ -0,0 +1,13 @@
+package demo.demotool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotoolApplication.class, args);
+    }
+
+}
---
+++ demo/src/test/java/demo/demotool/DemotoolApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.demotool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
