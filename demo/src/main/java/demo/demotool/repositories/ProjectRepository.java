5a25b51c2a48fd0a52a31a0c29656bff5e193a15
Yassinesbr <essebbar.yassine@gmail.com>
2019-12-13 00:08:36 +0100

    Project Object

---
+++ demo/src/main/java/demo/demotool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package demo.demotool.repositories;
+
+import demo.demotool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
---
+++ demo/src/main/java/demo/demotool/services/ProjectService.java
@@ -0,0 +1,19 @@
+package demo.demotool.services;
+
+import demo.demotool.domain.Project;
+import demo.demotool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        return projectRepository.save(project);
+    }
+
+}
